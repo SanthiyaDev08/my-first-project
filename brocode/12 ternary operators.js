192ee992c04e2ab73a2e998dda771f0902b2bd67
Santhiya-5048 <santhiyas24cse@kongu.edu>
2025-09-14 11:19:45 +0530

    Delete all files

--- brocode/12 ternary operators.js
+++
@@ -1,6 +0,0 @@
-let age = 25;
-let message = age >= 18 ? "You're an adult" : "You're minor";
-console.log(message);
-let purchaseAmount = 100;
-let discount = purchaseAmount >= 100 ? 10 : 0;
-console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
